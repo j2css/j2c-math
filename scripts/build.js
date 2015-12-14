93efeba01f036e29353901a937758a3192dec0a2
Pierre-Yves Gerardy <py@py.gy>
2015-12-14 02:26:33 +0100

    lint the scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,9 @@
 #! /usr/bin/env node
+/*eslint-env node*/
+/*eslint no-console: 0*/
+
 var fs = require('fs'),
   rollup = require('rollup'),
-  uglify = require('uglify-js'),
-  zlib = require('zlib'),
   outputs = [
     {
       rollupOptions: {
@@ -17,7 +18,7 @@ var fs = require('fs'),
         format: 'cjs'
       },
       name: 'commonjs',
-      minify: true // To test the minified version, 
+      minify: true // To test the minified version,
                    //in case it was too aggressibely crushed.
     },
     {
@@ -28,32 +29,17 @@ var fs = require('fs'),
       name: 'amd',
       minify: true
     }
-  ]
+  ];
 
 var parsed = rollup.rollup({
-  entry: "j2c-math.js"
-})
+  entry: 'j2c-math.js'
+});
 
 
 outputs.forEach(function (output) {
-  // console.log(output)
   parsed.then(function (bundle) {
     var result = bundle.generate(output.rollupOptions);
     fs.writeFileSync('dist/j2c-math.'  + output.name + '.js', result.code);
-    // if (output.minify) {
-    //   var minified = uglify.minify(
-    //     result.code, 
-    //     {
-    //       fromString: true,
-    //       comments: true
-    //     }
-    //   ).code;
-
-    //   fs.writeFileSync('dist/j2c-math.'  + output.name + '.min.js', minified);
-    //   zlib.gzip(minified, function(_, buf){ 
-    //     console.log(output.name, _ || buf.length);
-    //   });
-    // }
-  }).then(null, function (e) { console.log(output.name, e)});
+  }).then(null, function (e) {console.log(output.name, e);});
 });
 
